39c66fc8bb0e8d41553662ee1d2b3f1b88ac8f45
Jean-Philippe Caissy <jpcaissy@piji.ca>
2019-09-17 20:49:02 -0400

    Ajout exemple de station de vote

---
+++ cours/03-sept-18/exemples/poll/views.py
@@ -0,0 +1,86 @@
+from flask import escape
+
+def index(polls):
+    view = """
+<html>
+    <body>
+        <h1>Sondages</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nom</th>
+                </tr>
+            </thead>
+            <tbody>
+    """
+
+    for poll in polls:
+        view += """
+                <tr>
+                    <td>{id}</td>
+                    <td><a href="/polls/{id}">{name}</a></td>
+                </tr>
+        """.format(id=poll.id, name=poll.name)
+
+    return view + """</tbody></table><hr /><a href="/polls/new">Nouveau sondage</a></body></html>"""
+
+def index_empty():
+    return """
+<html>
+    <body>
+        <h1>Sondages</h1>
+        <h2>Aucuns sondages actifs</h2>
+
+        <a href="/polls/new">Nouveau sondage</a>
+    </body>
+</html>
+    """
+
+def new_poll(poll):
+    view = """
+<html>
+    <body>
+        <h1>Sondages</h1>
+        <h2>Nouveau</h2>
+
+        <form method="POST">
+            <label for="name">Nom : </label><input type="text" name="name" value="{name}" /><br />
+            <button type="submit">Créer</button>
+        </form>
+    </body>
+</html>
+    """.format(name=escape(poll.name))
+
+    return view
+
+def view_poll(poll, choices):
+    view = """
+<html>
+    <body>
+        <h1>Sondages</h1>
+        <h2>{name}</h2>
+        <p>{total_votes} vote(s) au total</p>
+    """.format(name=escape(poll.name), total_votes=poll.number_of_votes())
+
+    if choices:
+        view += """<form method="POST" action="/polls/{poll_id}/vote">""".format(poll_id=poll.id)
+        for choice in choices:
+            view += """
+            <label for="choice-{id}">{choice} (<strong>{votes} vote(s)</strong>): </label>
+            <input type="radio" name="choice_id" id="choice-{id}" value="{id}" />
+            <br />
+            """.format(id=choice.id, choice=choice.choice, votes=choice.number_of_votes())
+        view += """<button type="submit">Soumettre le vote</button></form>"""
+    else:
+        view += "Il n'y a aucun choix de disponible.<br />"
+
+    view += """
+    <hr />
+    <form method="POST" action="/polls/{poll_id}/choices/new">
+        <p>Rajouter un choix au sondage : </p>
+        <label for="choice">Choix : </label><input type="text" id="choice" name="choice" /><br />
+        <button type="submit">Rajouter</button>
+    </form>""".format(poll_id=poll.id)
+
+    return view + """<hr /><a href="/">Retourner à la liste des sondages</a></body></html>"""
